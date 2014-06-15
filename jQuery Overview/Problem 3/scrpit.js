68bcfc5495dc87d5d273174ff9a54cb72018c8ad
Georgi Prodanov <georgiwe@gmail.com>
2014-06-15 14:29:17 +0300

    Upload problem 3

---
+++ jQuery Overview/Problem 3/scrpit.js
@@ -0,0 +1,49 @@
+﻿var students = [{
+    firstName: 'Peter',
+    lastName: 'Ivanov',
+    grade: 3,
+}, {
+    firstName: 'Milena',
+    lastName: 'Grigorova',
+    grade: 6,
+}, {
+    firstName: 'Gergana',
+    lastName: 'Borisova',
+    grade: 12,
+}, {
+    firstName: 'Boyko',
+    lastName: 'Petrov',
+    grade: 7,
+}];
+
+function createTableByArrayOfStudents(students) {
+
+    var table = $('<table />');
+    var titleRow = $('<tr />');
+    var firstColHeading = $('<th />').text('First name');
+    var secondColHeading = $('<th />').text('Last name');
+    var thirdColHeading = $('<th />').text('Grade');
+
+    titleRow.append(firstColHeading);
+    titleRow.append(secondColHeading);
+    titleRow.append(thirdColHeading);
+    table.append(titleRow)
+
+    for (var i = 0; i < students.length; i++) {
+        var currRow = $('<tr />');
+
+        var firstNameCol = $('<td />').text(students[i].firstName);
+        var lastNameCol = $('<td />').text(students[i].lastName);
+        var gradeCol = $('<td />').text(students[i].grade);
+
+        currRow.append(firstNameCol);
+        currRow.append(lastNameCol);
+        currRow.append(gradeCol);
+
+        table.append(currRow);
+    }
+
+    $('body').append(table);
+}
+
+createTableByArrayOfStudents(students);
