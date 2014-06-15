aff9764fb9bd869d5400f518ee735f5f6ae45b58
Georgi Prodanov <georgiwe@gmail.com>
2014-06-15 14:00:15 +0300

    Upload Problem 2

---
+++ jQuery Overview/Problem 2/scripts/script.js
@@ -0,0 +1,27 @@
+﻿function insBefore(elemToInsert, insertBefore) {
+    $elemToInsert = $(elemToInsert);
+    $insertBefore = $(insertBefore);
+
+    $elemToInsert.insertBefore($insertBefore);
+}
+
+function insAfter(elemToInsert, insertAfter) {
+    $elemToInsert = $(elemToInsert);
+    $insertAfter = $(insertAfter);
+
+    $elemToInsert.insertAfter($insertAfter);
+}
+
+var $divTemplate = $('<div />');
+var $pivot = $('#pivot');
+var marker = 'last-inserted';
+var liveListClassInserted = document.getElementsByClassName(marker);
+
+$('nav').on('click', 'button', function () {
+    var attr = $(this).data('id');
+    $(liveListClassInserted[0]).removeClass(marker);
+    var $clone = $divTemplate.clone(true).addClass(marker);
+    (attr === 'before') ?
+        insBefore($clone, $pivot) :
+            insAfter($clone, $pivot);
+})
